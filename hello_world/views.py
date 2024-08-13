4cfca7194aa17ab2e8f1be147ee4ddce72fc4567
Kyle Mardell <kyleslufc@gmail.com>
2024-08-13 21:46:21 +0000

    forgot to save a file

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
-
-
-def about_me(request):
-    return HttpResponse("This would be the about page")
